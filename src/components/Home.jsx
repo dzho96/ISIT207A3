4eaa6148d566609cc2a0d8e87cfc0845edd88336
Emoclew0177 <82224948+Emoclew0177@users.noreply.github.com>
2026-02-07 17:33:38 +0800

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <h2>Home Page</h2>
+      <img src="https://media.newyorker.com/photos/5dfab39dde5fcf00086aec77/4:3/w_2271,h_1703,c_limit/Lane-Cats.jpg" alt="cat photo" />
+    </>
+
+  )
+}
+
+export default Home;
